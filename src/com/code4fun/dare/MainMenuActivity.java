f007a8c2c4a7e578a46da31be75c46f2300dd712
Vlad Victor Ungureanu <ungureanuvladvictor@gmail.com>
2013-11-23 16:19:42 +0100

    removed default launcher icon

---
+++ src/com/code4fun/dare/MainMenuActivity.java
@@ -0,0 +1,7 @@
+package com.code4fun.dare;
+
+/**
+ * Created by vvu on 23/11/13.
+ */
+public class MainMenuActivity {
+}
---
+++ src/dataFetch.java
@@ -0,0 +1,5 @@
+/**
+ * Created by vvu on 23/11/13.
+ */
+public class dataFetch {
+}
